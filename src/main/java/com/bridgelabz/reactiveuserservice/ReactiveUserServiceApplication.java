70dd688dff4f6741c5d46a6766140c51cf1579f1
Rohan2596 <rohankadam662@gmail.com>
2020-09-12 10:56:15 +0530

    STARTED:Reactive User Service of Applications.

---
+++ src/main/java/com/bridgelabz/reactiveuserservice/ReactiveUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.reactiveuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveUserServiceApplication.class, args);
+    }
+
+}
