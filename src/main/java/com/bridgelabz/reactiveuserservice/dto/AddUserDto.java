fb135a48cc6c7a218aa9e3583162614b8b8dca9e
Rohan2596 <rohankadam662@gmail.com>
2020-09-15 11:31:59 +0530

    ADDED:Mobile negative Test Case for null,empty,pattern & size.

--- src/main/java/com/bridgelabz/reactiveuserservice/dto/AddUserDto.java
+++ src/main/java/com/bridgelabz/reactiveuserservice/dto/AddUserDto.java
@@ -21,14 +21,16 @@ public class AddUserDto  {
     @NotNull(message = "Email Address can't be null.")
     @NotEmpty(message = "Email Address can't be empty.")
     @Email(message = "Email Address pattern doesn't match.")
-    @Pattern(regexp = "[a-zA-Z0-9.]{1,}+[@]{1}+[a-zA-Z0-9.]{1,}$",message = "Email id Pattern Does'nt match.")
+    @Pattern(regexp = "[a-zA-Z0-9.]{1,}+[@]{1}+[a-zA-Z0-9.]{1,}$",message = "Email id Pattern Doesn't match.")
     public String emailId;
 
     @NotNull(message = "Password can't be null.")
     @NotEmpty(message = "Password can't be empty.")
+    @Pattern(regexp = "[a-zA-Z0-9@#$%*]{8,25}",message = "Password Pattern must doesn't match and size 8 to 25.")
     public String password;
 
     @NotNull(message = "Mobile Number can't be null.")
     @NotEmpty(message = "Mobile Number can't be empty.")
+    @Pattern(regexp = "[789]{1}+[0-9]{9}",message = "Mobile Number Pattern doesn't match.")
     public String mobileNumber;
 }
--- src/test/java/com/bridgelabz/reactiveuserservice/controller/UserControllerTest.java
+++ src/test/java/com/bridgelabz/reactiveuserservice/controller/UserControllerTest.java
@@ -28,19 +28,19 @@ public class UserControllerTest {
     private AddUserDto addUserDto;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
 
     }
 
     /*
-    *@author ROHAN KADAM
-    * Purpose:User ADDITION/Register Test Cases For Negative and Positive use cases
-    * @date 14 September 2020
-    * */
+     *@author ROHAN KADAM
+     * Purpose:User ADDITION/Register Test Cases For Negative and Positive use cases
+     * @date 14 September 2020
+     * */
 
     @Test
-    public void givenValidUserDetails_whenAdded_shouldReturnCorrectResponse(){
-        this.addUserDto=new AddUserDto("Rohan Kadam",
+    public void givenValidUserDetails_whenAdded_shouldReturnCorrectResponse() {
+        this.addUserDto = new AddUserDto("Rohan Kadam",
                 "rohan.kadam@bridgelabz.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -54,11 +54,15 @@ public class UserControllerTest {
 
     }
 
+    /*
+     * User Name
+     * */
+
     @Test
-    public void givenInValidUserDetails_Name_null_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_Name_null_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto(null,
+        this.addUserDto = new AddUserDto(null,
                 "rohan.kadam@bridgelabz.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -72,10 +76,10 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_Name_Empty_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_Name_Empty_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("",
+        this.addUserDto = new AddUserDto("",
                 "rohan.kadam@bridgelabz.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -89,26 +93,10 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_Name_pattern_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_Name_pattern_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("11234",
-                "rohan.kadam@bridgelabz.com",
-                "BridgeLabz@2020",
-                "7890123456");
-        webTestClient.post().uri("/reactive/user")
-                .accept(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromObject(this.addUserDto))
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody();
-
-    }
-    @Test
-    public void givenInValidUserDetails_Name_sizeMin_whenAdded_shouldReturnCorrectResponse(){
-
-
-        this.addUserDto=new AddUserDto("ad",
+        this.addUserDto = new AddUserDto("11234",
                 "rohan.kadam@bridgelabz.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -122,10 +110,10 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_Name_sizeMax_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_Name_sizeMin_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("ad xdfdsfsdfsdsfsdsfddsfds sdfsdfsdfds dfsfs sdfsdfsdf esfsdfdsfdsfds sdfdsf",
+        this.addUserDto = new AddUserDto("ad",
                 "rohan.kadam@bridgelabz.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -139,10 +127,33 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_email_null_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_Name_sizeMax_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("Rohan Kadam",
+        this.addUserDto = new AddUserDto("ad xdfdsfsdfsdsfsdsfddsfds sdfsdfsdfds dfsfs sdfsdfsdf esfsdfdsfdsfds sdfdsf",
+                "rohan.kadam@bridgelabz.com",
+                "BridgeLabz@2020",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+
+    /*
+     * User Email Address
+     * */
+
+
+    @Test
+    public void givenInValidUserDetails_email_null_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("Rohan Kadam",
                 null,
                 "BridgeLabz@2020",
                 "7890123456");
@@ -156,10 +167,10 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_email_Empty_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_email_Empty_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("ROhan Kadam",
+        this.addUserDto = new AddUserDto("ROhan Kadam",
                 " ",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -173,10 +184,10 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_email_pattern1_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_email_pattern1_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("Rohan kadam",
+        this.addUserDto = new AddUserDto("Rohan kadam",
                 "rohan.bridgelabz.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -188,11 +199,12 @@ public class UserControllerTest {
                 .expectBody();
 
     }
+
     @Test
-    public void givenInValidUserDetails_email_pattern2_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_email_pattern2_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("ROhan kadam",
+        this.addUserDto = new AddUserDto("ROhan kadam",
                 "rohan.kadam@@@#.com",
                 "BridgeLabz@2020",
                 "7890123456");
@@ -206,13 +218,260 @@ public class UserControllerTest {
     }
 
     @Test
-    public void givenInValidUserDetails_email_pattern3_whenAdded_shouldReturnCorrectResponse(){
+    public void givenInValidUserDetails_email_pattern3_whenAdded_shouldReturnCorrectResponse() {
 
 
-        this.addUserDto=new AddUserDto("ROhan Kadam",
+        this.addUserDto = new AddUserDto("ROhan Kadam",
                 "rohan.kadam@bridgelabz@.@com",
                 "BridgeLabz@2020",
                 "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+
+
+    /*
+     * User Password
+     * */
+
+
+    @Test
+    public void givenInValidUserDetails_password_null_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("Rohan Kadam",
+                "rohan.kadam@bridgelabz.com",
+                null,
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_password_Empty_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com",
+                "",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_password_pattern1_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("Rohan kadam",
+                "rohan.bridgelabz.com",
+                "Qwert<>[]&&*",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_password_pattern2_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan kadam",
+                "rohan.kadam@bridgelabz.com",
+                "brider--**-",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_password_pattern3_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com.in",
+                "BridgeLabz~---875",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_password_sizeMin_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com.in",
+                "B@2020",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_password_sizeMax_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com.in",
+                "BridgeLabz@2020dsfksnfsfnsdf cxvcxvcxv zxczxczxcsssdfsdf",
+                "7890123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+
+    /*
+     * User  Mobile Number
+     * */
+
+
+    @Test
+    public void givenInValidUserDetails_MobileNumber_null_whenAdded_shouldReturnCorrectResponse() {
+
+        this.addUserDto = new AddUserDto("Rohan Kadam",
+                "rohan.kadam@bridgelabz.com",
+                "Bridgelabz@2020",
+                null);
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_MobileNumber_Empty_whenAdded_shouldReturnCorrectResponse() {
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com",
+                "Bridgelabz@2020",
+                " ");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_MobileNumber_pattern1_whenAdded_shouldReturnCorrectResponse() {
+
+        this.addUserDto = new AddUserDto("Rohan kadam",
+                "rohan.bridgelabz.com",
+                "Bridgelabz@2020",
+                "7123qwetry");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_MobileNumber_pattern2_whenAdded_shouldReturnCorrectResponse() {
+
+        this.addUserDto = new AddUserDto("ROhan kadam",
+                "rohan.kadam@bridgelabz.com",
+                "Bridgelabz@2020",
+                "12345qwert");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_MobileNumber_pattern3_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com.in",
+                "BridgeLabz@2020",
+                "78o0123456");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_mobileNumber_sizeMin_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com.in",
+                "Bridgelabz@2020",
+                "7890");
+        webTestClient.post().uri("/reactive/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromObject(this.addUserDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody();
+
+    }
+
+    @Test
+    public void givenInValidUserDetails_mobileNumber_sizeMax_whenAdded_shouldReturnCorrectResponse() {
+
+
+        this.addUserDto = new AddUserDto("ROhan Kadam",
+                "rohan.kadam@bridgelabz.com.in",
+                "BridgeLabz@2020",
+                "789012345613123");
         webTestClient.post().uri("/reactive/user")
                 .accept(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromObject(this.addUserDto))
@@ -229,7 +488,7 @@ public class UserControllerTest {
      * */
 
     @Test
-    public void givenValidUserDetails_whenAuthenticated_shouldReturnCorrectResponse(){
+    public void givenValidUserDetails_whenAuthenticated_shouldReturnCorrectResponse() {
 
         webTestClient.post().uri("/reactive/user/auth")
                 .accept(MediaType.APPLICATION_JSON_UTF8)
@@ -247,7 +506,7 @@ public class UserControllerTest {
      * */
 
     @Test
-    public void givenValidUserToken_whenVerified_shouldReturnCorrectResponse(){
+    public void givenValidUserToken_whenVerified_shouldReturnCorrectResponse() {
         webTestClient.get().uri("/reactive/user/verify")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -264,7 +523,7 @@ public class UserControllerTest {
      * */
 
     @Test
-    public void givenValidUserToken_whenForgot_shouldReturnCorrectResponse(){
+    public void givenValidUserToken_whenForgot_shouldReturnCorrectResponse() {
         webTestClient.get().uri("/reactive/user/forgot")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -281,7 +540,7 @@ public class UserControllerTest {
      * */
 
     @Test
-    public void givenValidResetDetails_whenUpdated_shouldReturnCorrectResponse(){
+    public void givenValidResetDetails_whenUpdated_shouldReturnCorrectResponse() {
         webTestClient.post().uri("/reactive/user/reset")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -297,16 +556,16 @@ public class UserControllerTest {
      * */
 
     @Test
-    public void givenValidInput_whenVerified_shouldReturnListOfAllUser(){
-        List<String> list= new ArrayList<>();
+    public void givenValidInput_whenVerified_shouldReturnListOfAllUser() {
+        List<String> list = new ArrayList<>();
         list.add("John Doe");
         list.add("John Hopkins");
-      webTestClient.get().uri("/reactive/user/all")
+        webTestClient.get().uri("/reactive/user/all")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class)
-               .isEqualTo(list.get(0));
+                .isEqualTo(list.get(0));
 
     }
 
